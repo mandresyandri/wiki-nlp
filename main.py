042e6a23f343663861531848aff02a01aff22d59
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2023-11-18 15:38:27 +0000

    verifying urls

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# importing modules
+# Importing modules
 import requests
 from bs4 import BeautifulSoup
 import pandas as ps
 
 # Base function
 def main(surburl : str):
-    # base url 
+    # Testing the user inputs
+    
+    # Setting the search
     base_url = "https://fr.wikipedia.org/wiki/"
     url = base_url + surburl
 
-    return url
+    # Verifying the url
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Erreur : {response.status_code}")
 
-# execute function
+    return ""
+
+# Execute function
 if __name__ == "__main__":
     user_input = input("Votre recherche : ")
     print(f"{main(user_input)} \n")
-
